e32acf89e366d2d39a39c82b97b2ef68c427cbff
saikava <niakro@mail.ru>
2018-07-02 21:28:39 +0300

    font

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
+import Home from './Containers/Home/Home';
 
 const App = () => (
     <Switch>
-        <Route exact path='/' component={() => 'home'}/>
+        <Route exact path='/' component={Home}/>
         <Route path='/test' component={() => 'test'}/>
     </Switch>
 );
