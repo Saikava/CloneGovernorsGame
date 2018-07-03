53431126a347b5c40597651966194963dc608f34
saikava <niakro@mail.ru>
2018-07-03 23:08:46 +0300

    Modal age

--- src/Components/Modal/Modal.jsx
+++ src/Components/Modal/Modal.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import s from './Modal.scss';
+import { getAge } from "../../Helpers/formatHelper";
 
 const Modal = ({ card, onAction }) => (
     <div className={s.container}>
         <div className={s.content}>
             <img src={card.imagePath} />
             <div className={s.title}>
-                {card.name}
+                {card.name}, {getAge(card.birthday)}
             </div>
             <div className={s.subtitle}>
                 {card.subtitle}
--- src/Helpers/formatHelper.js
+++ src/Helpers/formatHelper.js
@@ -7,3 +7,21 @@ export function formatMoves(n) {
 
     return `${n} ${units}`;
 }
+
+export function getAge(birthday) {
+    const n = new Date().getFullYear() - parseInt(birthday.slice(0,4));
+    let units = 'лет';
+
+    if (Math.floor(n % 100 / 10) !== 1
+        && n % 10 > 0
+        && n % 10 < 5
+    ) {
+        if (n % 10 === 1) {
+            units = 'год';
+        } else {
+            units = 'года';
+        }
+    }
+
+    return `${n} ${units}`;
+}
